3ca410b422765762223c7f09c3574a8820b849ec
Tonmoy298 <tonmoyofs@gmail.com>
2026-05-15 12:11:56 +0600

    Add files via upload

---
+++ SolidPatern.java
@@ -0,0 +1,26 @@
+
+package solidpatern;
+import java.util.*;
+public class SolidPatern {
+
+   
+    public static void main(String[] args) {
+     Scanner obj=new Scanner (System.in);
+     System.out.println("Enter number of n:");
+       int n = obj. nextInt() ;
+      System.out.println("Enter number of m:");
+       int m = obj .nextInt() ;
+      for(int i=1;i<=n;i++) {
+      for(int j=1;j<=m;j++){
+          
+       System.out.print("*");   
+      }
+      System.out.println(""); 
+      
+      }
+      
+        
+        
+    }
+    
+}
